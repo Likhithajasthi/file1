379f3c22ce53c863150472e52e4c1b587e21fa4a
Likhithajasthi <likhithajasti11@gmail.com>
2016-12-27 19:15:07 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -1,3 +1,4 @@
-anusha
-mamidala
-likhitha
+Charles Hodge (1797–1878) was a Presbyterian theologian and principal of Princeton Theological Seminary between 1851 and 1878.
+He was a leading exponent of the Princeton theology, an orthodox Calvinist theological tradition in America during the 
+19th century. He argued strongly for the authority of the Bible as the Word of God. 
+Many of his ideas were adopted in the 20th century by Fundamentalists and Evangelicals.
